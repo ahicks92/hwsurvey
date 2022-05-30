0424c87c23f1234b8b8b9bfa112aae75179187e1
Austin Hicks <ahicks@ahicks.io>
2022-05-29 20:38:14 -0700

    Bring in line with simdsp again, so that we get the namespaced fields and architecture name

--- client/src/simdsp_bridge.rs
+++ client/src/simdsp_bridge.rs
@@ -5,26 +5,27 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SystemInfo {
     pub cpu_manufacturer: String,
+    pub cpu_architecture: String,
     pub cache_info: CacheInfo,
     pub cpu_capabilities: CpuCapabilities,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CpuCapabilities {
-    pub sse2: bool,
-    pub sse3: bool,
-    pub ssse3: bool,
-    pub sse4_1: bool,
-    pub popcnt_insn: bool,
-    pub avx: bool,
-    pub avx2: bool,
-    pub fma3: bool,
-    pub fma4: bool,
-    pub xop: bool,
-    pub avx512f: bool,
-    pub avx512bw: bool,
-    pub avx512dq: bool,
-    pub avx512vl: bool,
+    pub x86_sse2: bool,
+    pub x86_sse3: bool,
+    pub x86_ssse3: bool,
+    pub x86_sse4_1: bool,
+    pub x86_popcnt_insn: bool,
+    pub x86_avx: bool,
+    pub x86_avx2: bool,
+    pub x86_fma3: bool,
+    pub x86_fma4: bool,
+    pub x86_xop: bool,
+    pub x86_avx512f: bool,
+    pub x86_avx512bw: bool,
+    pub x86_avx512dq: bool,
+    pub x86_avx512vl: bool,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
