913541369a3bc355fcc2ae3b550335a649092cf1
Austin Hicks <ahicks@ahicks.io>
2022-06-05 11:18:39 -0700

    server: warp filters to extract IP and country

--- server/src/api/report_v1.rs
+++ server/src/api/report_v1.rs
@@ -8,15 +8,34 @@ use hwsurvey_payloads::PayloadV1;
 
 use crate::writer::WriterThread;
 
-pub async fn report_v1_fallible(writer: &WriterThread, body: Bytes) -> Result<()> {
+/// Sentinel value for when we can't extract an IP, which should almost never happen.
+pub const UNKNOWN_IP_STRING: &str = "unknown_ip";
+
+/// The unknown country from Cloudflare's docs:
+/// https://developers.cloudflare.com/fundamentals/get-started/reference/http-request-headers/
+///
+/// We fill this in automatically for local development or if the header is missing.
+pub const UNKNOWN_COUNTRY: [char; 2] = ['X', 'X'];
+
+pub async fn report_v1_fallible(
+    writer: &WriterThread,
+    _ip: Option<String>,
+    _country: Option<String>,
+    body: Bytes,
+) -> Result<()> {
     let payload: PayloadV1 = serde_json::from_slice(&body[..])?;
 
     writer.send(payload)?;
     Ok(())
 }
 
-pub async fn report_v1(writer: Arc<WriterThread>, body: Bytes) -> impl warp::reply::Reply {
-    let status = match report_v1_fallible(&*writer, body).await {
+pub async fn report_v1(
+    writer: Arc<WriterThread>,
+    ip: Option<String>,
+    country: Option<String>,
+    body: Bytes,
+) -> impl warp::reply::Reply {
+    let status = match report_v1_fallible(&*writer, ip, country, body).await {
         Ok(_) => warp::http::StatusCode::OK,
         Err(e) => {
             only_every::only_every!(Duration::from_secs(3), {
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,9 @@
-mod api;
 mod anonymization;
+mod api;
 mod writer;
 
 use std::str::FromStr;
+use std::time::Duration;
 
 use anyhow::Result;
 use clap::Parser;
@@ -19,8 +20,6 @@ struct Args {
     port: u16,
 }
 
-
-
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<()> {
     env_logger::init();
@@ -32,11 +31,46 @@ async fn main() -> Result<()> {
 
     let writer = writer::spawn();
 
+    let cf_ip = warp::header("CF-Connecting-IP").map(|x: String| Some(x));
+    let remote_ip = warp::filters::addr::remote().map(|x: Option<std::net::SocketAddr>| {
+        // This half of the filter only matches if the cloudflare header wasn't present, so log here.
+        //
+        // Also, if we got here and didn't get the ip at all, something is weird about our deployment, so log that too.
+        only_every::only_every!(
+            Duration::from_secs(30),
+            log::warn!("Unable to get IP from cloudflare. Falling back to remote addr")
+        );
+        if x.is_none() {
+            only_every::only_every!(
+                Duration::from_secs(30),
+                log::error!("Warp is unable to extract the remote address")
+            );
+        }
+        x.map(|y| y.ip().to_string())
+    });
+    let ip_filter = cf_ip.or(remote_ip).unify();
+
+    // Extract a country code as a 2-character array.
+    let country_filter =
+        warp::filters::header::optional::<String>("CF-IPCountry").map(|x: Option<String>| {
+            if x.is_none() {
+                only_every::only_every!(
+                    Duration::from_secs(30),
+                    log::warn!("Missing CF-IPCountry header")
+                );
+            }
+            x
+        });
+
     let report = warp::path!("report" / "v1")
         .and(warp::post())
         .and(warp::filters::body::content_length_limit(1024 * 10))
+        .and(ip_filter)
+        .and(country_filter)
         .and(warp::filters::body::bytes())
-        .then(move |b| api::report_v1::report_v1(writer.clone(), b));
+        .then(move |ip, country, body| {
+            api::report_v1::report_v1(writer.clone(), ip, country, body)
+        });
 
     warp::serve(report).run((ip, args.port)).await;
     Ok(())
